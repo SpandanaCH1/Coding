ae864e3a8476e0964dbafaf431bc6ea0bf51d345
Spandana <spandanachintalapudi1@gmail.com>
2025-12-26 15:10:26 +0530

    Add files via upload

---
+++ solution.java
@@ -0,0 +1,46 @@
+import java.util.*;
+public class solution
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int N;
+        N=sc.nextInt();
+        int [] arr=new int[N];
+        for(int i=0;i<N;i++)
+        {
+            arr[i] = sc.nextInt();
+        }
+        int max=0;
+        for(int i=0;i<N;i++)
+        {
+            if(max<arr[i])
+                max=arr[i];    
+        }
+        int [] flag=new int[max+1];
+        for(int j=0;j<max;j++)
+        {
+            flag[j]=0;
+        }
+        int MissedCount=0;
+        int [] missedEles= new int[max];
+        for(int j=1;j<max;j++)
+        {
+            if(flag[j]==0)
+            {
+                missedEles[MissedCount]=j;
+                MissedCount=MissedCount+1;
+            }
+        }
+        if(MissedCount==0)
+        {
+            System.out.println("No missed Elements");
+            return;
+        }
+        for(int k=0;k<MissedCount;k++)
+        {
+            System.out.println(missedEles[k]);
+        }
+    }
+    
+}
